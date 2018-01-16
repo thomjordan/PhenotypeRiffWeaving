7c99ed98502dd4d8d2a76eb5b11c786e8379b165
ThomJordan <thomjordan@users.noreply.github.com>
2018-01-16 14:11:21 -0500

    Initial Commit

---
+++ PhenotypeRiffWeaving/PhenotypeRiffWeaving.h
@@ -0,0 +1,19 @@
+//
+//  PhenotypeRiffWeaving.h
+//  PhenotypeRiffWeaving
+//
+//  Created by Thom Jordan on 1/16/18.
+//  Copyright © 2018 Thom Jordan. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for PhenotypeRiffWeaving.
+FOUNDATION_EXPORT double PhenotypeRiffWeavingVersionNumber;
+
+//! Project version string for PhenotypeRiffWeaving.
+FOUNDATION_EXPORT const unsigned char PhenotypeRiffWeavingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PhenotypeRiffWeaving/PublicHeader.h>
+
+
